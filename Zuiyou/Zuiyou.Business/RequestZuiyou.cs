a1baee3b587cab2cd1d41a28cf67bd0a2c3a2966
Inighty <mcshr@hotmail.com>
2016-07-27 16:27:21 +0800

    Clean

---
+++ Zuiyou/Zuiyou.Business/RequestZuiyou.cs
@@ -0,0 +1,429 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Better517Na.Http.Helper;
+using Better517Na.Json.Linq;
+using Better517Na.Proxy.Helper;
+using Zuiyou.Common;
+using Zuiyou.DAL;
+using Zuiyou.Model;
+
+namespace Zuiyou.Business
+{
+    /// <summary>
+    /// 业务请求类
+    /// </summary>
+    public class RequestZuiyou
+    {
+        ////string token = string.Empty;
+
+        ////public string Token
+        ////{
+        ////    get { return token; }
+        ////    set { token = value; }
+        ////}
+        ////string mid = string.Empty;
+
+        ////public string Mid
+        ////{
+        ////    get { return mid; }
+        ////    set { mid = value; }
+        ////}
+
+        /////// <summary>
+        /////// 构造函数
+        /////// </summary>
+        ////public RequestZuiyou()
+        ////{
+        ////    HttpRequestParam paramToken = new HttpRequestParam();
+        ////    paramToken.URL = "http://tbapi.ixiaochuan.cn/account/register_guest";
+        ////    paramToken.Method = "POST";
+        ////    paramToken.Postdata = string.Format("{{\"h_ts\":{0},\"h_did\":\"864394100860115_08:60:6E\"}}", (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000);
+        ////    paramToken.UserAgent = "tieba/20160715.184411(iPhone;IOS 10.0;Scale/2.00)";
+        ////    HttpResult resultToken = HttpHelper.GetHttpRequestData(paramToken);
+        ////    JObject tokenObj = JObject.Parse(resultToken.Html);
+        ////    if (tokenObj["data"] != null)
+        ////    {
+        ////        this.token = tokenObj["data"]["token"].ToString();
+        ////        this.mid = tokenObj["data"]["mid"].ToString();
+        ////    }
+        ////}
+
+        /// <summary>
+        /// 起始点
+        /// </summary>
+        private static int startpoint = ConfigReader.StartPoint;
+
+        /// <summary>
+        /// 线程数
+        /// </summary>
+        private static int threadNum = ConfigReader.ThreadNum;
+
+        /// <summary>
+        /// 终结点
+        /// </summary>
+        private static int endpoint = ConfigReader.EndPoint;
+
+        /// <summary>
+        /// 表名
+        /// </summary>
+        private static string table = "zuiyoulist";
+
+        /// <summary>
+        /// 初始化随机数
+        /// </summary>
+        private static string checkCode = "0001";
+
+        /// <summary>
+        /// 实例化数据操作类
+        /// </summary>
+        private SaveData saveData = new SaveData();
+
+        /// <summary>
+        /// 接收数据，执行方法
+        /// </summary>
+        /// <param name="o">数据对象</param>
+        public static void Method(object o)
+        {
+            ////此处对传进来的参数进行处理
+            int[] p = (int[])o;
+
+            ////调用GetPostEnum方法
+            RequestZuiyou request = new RequestZuiyou();
+            request.GetPostEnum(p[0], p[1]);
+        }
+
+        /// <summary>
+        /// unix时间戳转DateTime
+        /// </summary>
+        /// <param name="unixTimeStamp">时间戳</param>
+        /// <returns>DateTime</returns>
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // 定义其实时间
+            System.DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dt = dt.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dt;
+        }
+
+        /// <summary>
+        /// 请求数据，解析返回数据存入数据库（每次返回12条数据左右）
+        /// </summary>
+        public void Zuiyou()
+        {
+            int count = 0;
+            string sql = string.Empty;
+            List<PosterModel> list = new List<PosterModel>();
+            while (true)
+            {
+                try
+                {
+                    list = this.GetPost();
+                    if (list.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    sql = this.ListToSql(list, table);
+                    if (string.IsNullOrEmpty(sql))
+                    {
+                        continue;
+                    }
+
+                    count = this.saveData.getmysqlcom(sql);
+                    ////Console.WriteLine("更新或添加成功" + count + "条数据");
+                    Thread.Sleep(2000);
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// 根据起始结束点循环执行请求，解析返回数据存入数据库（根据帖子ID递增请求）
+        /// </summary>
+        /// <param name="start">起点</param>
+        /// <param name="end">终点</param>
+        public void GetPostEnum(int start, int end)
+        {
+            try
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    List<PosterModel> posterList = new List<PosterModel>();
+                    HttpResult result = null;
+                    HttpRequestParam param = new HttpRequestParam();
+                    param.URL = "http://tbapi.ixiaochuan.cn/post/detail";
+                    param.Method = "POST";
+
+                    ////param.Postdata = "{\"h_ts\":" + ((DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000) + ",\"h_av\":\"2.6.0\",\"h_nt\":1,\"h_m\":0,\"h_did\":\""+GenerateCheckCode()+"_08:60:7E\"}";
+                    param.Postdata = "{\"h_dt\":0,\"h_av\":\"2.6.0\",\"pid\":" + i + ",\"from\":\"index\",\"h_nt\":1}";
+                    param.KeepAlive = true;
+                    param.UserAgent = "tieba/20160715.184411(iPhone;IOS 10.0;Scale/2.00)";
+                    ////proxyInfo = ProxyHelper.GetProxyInfo("Rightest_Grab");
+                    try
+                    {
+                        result = HttpHelper.GetHttpRequestData(param);
+                    }
+                    catch
+                    {
+                    }
+
+                    if (result != null)
+                    {
+                        JObject obj = JObject.Parse(result.Html);
+                        if (obj["ret"].ToString() == "1" && obj["data"] != null)
+                        {
+                            Console.WriteLine(i);
+                            JToken data = obj["data"]["post"];
+                            posterList.Add(this.GetModel(data));
+                        }
+
+                        if (posterList.Count == 0)
+                        {
+                            continue;
+                        }
+
+                        string sql = this.ListToSql(posterList, table);
+                        int count = this.saveData.getmysqlcom(sql);
+                    }
+
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 请求返回list
+        /// </summary>
+        /// <returns>返回list</returns>
+        public List<PosterModel> GetPost()
+        {
+            List<PosterModel> posterList = new List<PosterModel>();
+            HttpResult result = null;
+            try
+            {
+                HttpRequestParam param = new HttpRequestParam();
+                ////MProxyInfo proxyInfo = null;
+                param.URL = "http://tbapi.ixiaochuan.cn/index/recommend";
+                param.Method = "POST";
+
+                ////param.Postdata = "{\"h_ts\":" + ((DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000) + ",\"h_av\":\"2.6.0\",\"h_nt\":1,\"h_m\":0,\"h_did\":\""+GenerateCheckCode()+"_08:60:7E\"}";
+                param.Postdata = "{\"h_ts\":" + ((DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000) + ",\"h_av\":\"2.6.0\",\"h_nt\":1,\"h_m\":0,\"h_did\":\"86439410086" + checkCode + "_08:60:7E\"}";
+                param.KeepAlive = true;
+                param.UserAgent = "tieba/20160715.184411(iPhone;IOS 10.0;Scale/2.00)";
+                ////proxyInfo = ProxyHelper.GetProxyInfo("Rightest_Grab");
+                ////if (proxyInfo != null)
+                ////{
+                ////    param.ProxyIP = proxyInfo.IP + ":" + proxyInfo.Port;
+                ////    param.ProxyUserName = proxyInfo.UserName;
+                ////    param.ProxyPwd = proxyInfo.Password;
+                ////    param.ProxyBusinessName = "Rightest_Grab";
+                ////}
+                try
+                {
+                    result = HttpHelper.GetHttpRequestData(param);
+                }
+                catch
+                {
+                }
+
+                if (result != null)
+                {
+                    JObject obj = JObject.Parse(result.Html);
+                    if (obj["ret"].ToString() == "1" && obj["data"] != null)
+                    {
+                        JArray datalist = obj["data"]["list"] as JArray;
+                        foreach (JToken item in datalist)
+                        {
+                            PosterModel model = new PosterModel();
+                            model = this.GetModel(item);
+                            posterList.Add(model);
+                        }
+
+                        Console.WriteLine(DateTime.Now + " 获取到" + datalist.Count + "条数据");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+
+            return posterList;
+        }
+
+        /// <summary>
+        /// 根据返回数据解析为model
+        /// </summary>
+        /// <param name="item">请求返回的数据</param>
+        /// <returns>model</returns>
+        public PosterModel GetModel(JToken item)
+        {
+            PosterModel model = new PosterModel();
+            model.Id = Convert.ToInt32(item["id"]);
+            model.Mid = Convert.ToInt32(item["member"]["id"]);
+            model.Name = item["member"]["name"].ToString();
+            model.Gender = Convert.ToInt32(item["member"]["gender"]);
+            model.Content = item["content"].ToString();
+            if (item["imgs"] != null && item["imgs"].ToArray().Length != 0)
+            {
+                JArray imglist = item["imgs"] as JArray;
+                foreach (var img in imglist)
+                {
+                    model.Img += string.Format("http://tbfile.ixiaochuan.cn/img/view/id/{0}/sz/src,", img["id"].ToString());
+                    JObject imgDetail = JObject.Parse(img.ToString());
+                    try
+                    {
+                        if (imgDetail["video"] != null)
+                        {
+                            if (item["videos"] != null && item["videos"].ToArray().Length != 0 && imgDetail["video"].ToString() == "1")
+                            {
+                                model.Video += item["videos"][img["id"].ToString()]["url"].ToString() + ",";
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Write(item);
+                    }
+                }
+
+                model.Img = model.Img.Remove(model.Img.Length - 1);
+                model.Video = string.IsNullOrEmpty(model.Video) ? string.Empty : model.Video.Remove(model.Video.Length - 1);
+            }
+
+            model.Reviews = Convert.ToInt32(item["reviews"]);
+            model.Likes = Convert.ToInt32(item["likes"]);
+            model.Up = Convert.ToInt32(item["up"]);
+            model.Down = Convert.ToInt32(item["down"]);
+            model.Ctime = UnixTimeStampToDateTime(double.Parse(item["ct"].ToString()));
+            model.Topic = item["topic"]["topic"].ToString();
+            if (item["god_review"] != null && item["god_review"].ToArray().Length != 0)
+            {
+                model.God_review_id = Convert.ToInt32(item["god_review"]["id"]);
+                model.God_review_mid = Convert.ToInt32(item["god_review"]["mid"]);
+                model.God_review_name = item["god_review"]["mname"].ToString();
+                model.God_review_gender = Convert.ToInt32(item["god_review"]["gender"]);
+                model.God_review_content = item["god_review"]["review"].ToString();
+                if (item["god_review"]["imgs"] != null && item["god_review"]["imgs"].ToArray().Length != 0)
+                {
+                    JArray imglist = item["god_review"]["imgs"] as JArray;
+                    foreach (var img in imglist)
+                    {
+                        model.God_review_img += string.Format("http://tbfile.ixiaochuan.cn/img/view/id/{0}/sz/src,", img["id"].ToString());
+                        JObject imgDetail = JObject.Parse(img.ToString());
+                        if (imgDetail["video"] != null)
+                        {
+                            if (item["god_review"]["videos"] != null && item["god_review"]["videos"].ToArray().Length != 0 && imgDetail["video"].ToString() == "1")
+                            {
+                                model.God_review_video += item["god_review"]["videos"][img["id"].ToString()]["url"].ToString() + ",";
+                            }
+                        }
+                    }
+
+                    model.God_review_img = model.God_review_img.Remove(model.God_review_img.Length - 1);
+                    model.God_review_video = string.IsNullOrEmpty(model.God_review_video) ? string.Empty : model.God_review_video.Remove(model.God_review_video.Length - 1);
+                }
+
+                model.God_review_likes = Convert.ToInt32(item["god_review"]["likes"]);
+                model.God_review_up = Convert.ToInt32(item["god_review"]["up"]);
+                model.God_review_down = Convert.ToInt32(item["god_review"]["down"]);
+                model.God_review_ctime = UnixTimeStampToDateTime(double.Parse(item["god_review"]["ct"].ToString()));
+            }
+
+            return model;
+        }
+
+        /// <summary>
+        /// 分配任务
+        /// </summary>
+        public void FenPeiTask()
+        {
+            for (int i = 0; i < threadNum; i++)
+            {
+                int yu = (endpoint - startpoint) % threadNum;
+                int shang = (endpoint - startpoint) / threadNum;
+                int start = startpoint + ((endpoint - startpoint) / threadNum * i);
+                int end = yu != 0 && i == threadNum - 1 ? (shang * (i + 1)) + yu : shang * (i + 1);
+                ////为了能传递多个参数给线程，将两个数据封装为一个int[]传递
+                int[] para = new int[2] { start, end };
+                Thread enumThread = new Thread(new ParameterizedThreadStart(Method));
+                enumThread.Start(para);
+            }
+        }
+
+        /// <summary>
+        /// 产生4位的随机字符串
+        /// </summary>
+        public void GenerateCheckCode()
+        {
+            while (true)
+            {
+                int number;
+                char code;
+
+                System.Random random = new Random();
+
+                for (int i = 0; i < 4; i++)
+                {
+                    number = random.Next();
+                    code = (char)('0' + (char)(number % 10));
+                    checkCode += code.ToString();
+                }
+
+                Thread.Sleep(TimeSpan.FromMinutes(2));
+            }
+        }
+
+        /// <summary>
+        /// List转为sql语句
+        /// </summary>
+        /// <param name="list">数据列表</param>
+        /// <param name="table">表名</param>
+        /// <returns>sql语句</returns>
+        public string ListToSql(List<PosterModel> list, string table)
+        {
+            StringBuilder sqlStr = new StringBuilder();
+            foreach (var item in list)
+            {
+                string names = string.Empty;
+                string values = string.Empty;
+                string updateStr = string.Empty;
+                Type t = item.GetType();
+                foreach (PropertyInfo pi in t.GetProperties())
+                {
+                    string name = pi.Name;
+                    names += name + ",";
+                    ////用pi.GetValue获得值
+                    object value1 = pi.GetValue(item, null);
+                    if (value1.ToString().Contains("'"))
+                    {
+                        value1 = value1.ToString().Replace("'", "''");
+                    }
+
+                    values += "'" + value1.ToString() + "'" + ",";
+                    updateStr += name + "=" + "'" + value1 + "',";
+                }
+
+                values = values.Substring(0, values.Length - 1);
+                names = names.Substring(0, names.Length - 1);
+                updateStr = updateStr.Substring(0, updateStr.Length - 1);
+                sqlStr.AppendFormat("insert into {0}({1}) values ({2}) ON DUPLICATE KEY UPDATE {3};\n", table, names, values, updateStr);
+            }
+
+            return sqlStr.ToString();
+        }
+    }
+}
---
+++ Zuiyou/Zuiyou.Model/PosterModel.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zuiyou.Model
+{
+    public class PosterModel
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private int mid;
+
+        public int Mid
+        {
+            get { return mid; }
+            set { mid = value; }
+        }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int gender;
+
+        public int Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        private string content;
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+        private string img = string.Empty;
+
+        public string Img
+        {
+            get { return img; }
+            set { img = value; }
+        }
+        private string video = string.Empty;
+
+        public string Video
+        {
+            get { return video; }
+            set { video = value; }
+        }
+
+        private int reviews;
+
+        public int Reviews
+        {
+            get { return reviews; }
+            set { reviews = value; }
+        }
+        private int likes;
+
+        public int Likes
+        {
+            get { return likes; }
+            set { likes = value; }
+        }
+        private int up;
+
+        public int Up
+        {
+            get { return up; }
+            set { up = value; }
+        }
+        private int down;
+
+        public int Down
+        {
+            get { return down; }
+            set { down = value; }
+        }
+        private DateTime ctime;
+
+        public DateTime Ctime
+        {
+            get { return ctime; }
+            set { ctime = value; }
+        }
+        private string topic = string.Empty;
+
+        public string Topic
+        {
+            get { return topic; }
+            set { topic = value; }
+        }
+        private int god_review_id;
+
+        public int God_review_id
+        {
+            get { return god_review_id; }
+            set { god_review_id = value; }
+        }
+        private int god_review_mid;
+
+        public int God_review_mid
+        {
+            get { return god_review_mid; }
+            set { god_review_mid = value; }
+        }
+        private string god_review_name = string.Empty;
+
+        public string God_review_name
+        {
+            get { return god_review_name; }
+            set { god_review_name = value; }
+        }
+
+        private int god_review_gender;
+
+        public int God_review_gender
+        {
+            get { return god_review_gender; }
+            set { god_review_gender = value; }
+        }
+        private string god_review_content = string.Empty;
+
+        public string God_review_content
+        {
+            get { return god_review_content; }
+            set { god_review_content = value; }
+        }
+
+        private string god_review_img = string.Empty;
+
+        public string God_review_img
+        {
+            get { return god_review_img; }
+            set { god_review_img = value; }
+        }
+
+
+        private string god_review_video = string.Empty;
+
+        public string God_review_video
+        {
+            get { return god_review_video; }
+            set { god_review_video = value; }
+        }
+
+
+        private int god_review_likes;
+
+        public int God_review_likes
+        {
+            get { return god_review_likes; }
+            set { god_review_likes = value; }
+        }
+        private int god_review_up;
+
+        public int God_review_up
+        {
+            get { return god_review_up; }
+            set { god_review_up = value; }
+        }
+        private int god_review_down;
+
+        public int God_review_down
+        {
+            get { return god_review_down; }
+            set { god_review_down = value; }
+        }
+        private DateTime god_review_ctime;
+
+        public DateTime God_review_ctime
+        {
+            get { return god_review_ctime; }
+            set { god_review_ctime = value; }
+        }
+
+
+    }
+}
